3ea149ed3856c6925924ac0ad412fa296ba21bee
reny <renypacheco08.rp@gmail.com>
2023-02-05 22:21:41 +0800

    feat: add config for react project

--- index.js
+++ index.js
@@ -18,13 +18,24 @@ const promptPackageManager = async () => {
   return packageManager;
 };
 
-const addConfigs = async () => {
+const promptProjectType = async () => {
+  const { projectType } = await inquirer.prompt({
+    name: 'projectType',
+    type: 'list',
+    message: 'What is your project type?',
+    choices: ['node', 'react'],
+    default: 'node'
+  });
+  return projectType;
+};
+
+const addConfigs = async (projectType) => {
   const __filename = fileURLToPath(import.meta.url);
   const PROJECT_PATH = path.dirname(__filename);
   const spinner = ora('Copying config files').start();
 
   try {
-    const configPath = path.join(PROJECT_PATH, 'config');
+    const configPath = path.join(PROJECT_PATH, 'config', projectType);
     await fs.copy(configPath, PROJECT_PATH);
     spinner.succeed('Copying config files completed!');
   } catch (err) {
@@ -34,7 +45,7 @@ const addConfigs = async () => {
   }
 };
 
-const installDevDependencies = async (pkgManager, devDependencies) => {
+const installDevDependencies = async (pkgManager, projectType, devDependencies) => {
   const spinner = ora('Installing dev dependencies!').start();
   try {
     const pkgCommand = pkgManager === 'npm' ? 'install' : 'add';
@@ -61,6 +72,19 @@ const installDevDependencies = async (pkgManager, devDependencies) => {
     await execa(pkgManager, [pkgCommand, devCommand, devDependencies[5]], {
       cwd: process.cwd()
     });
+
+    if (projectType === 'react') {
+      await execa(pkgManager, [pkgCommand, devCommand, devDependencies[6]], {
+        cwd: process.cwd()
+      });
+      await execa(pkgManager, [pkgCommand, devCommand, devDependencies[7]], {
+        cwd: process.cwd()
+      });
+      await execa(pkgManager, [pkgCommand, devCommand, devDependencies[8]], {
+        cwd: process.cwd()
+      });
+    }
+
     spinner.succeed('Dev dependencies installed successfully!');
   } catch (err) {
     spinner.fail('Error while installing dev dependencies!');
@@ -69,9 +93,10 @@ const installDevDependencies = async (pkgManager, devDependencies) => {
   }
 };
 
-const displayDevDependencies = (devDependencies) => {
+const displayDevDependencies = (projectType, devDependencies) => {
   console.log('\n-----[ Installed dev dependencies ]-----\n');
-  devDependencies.forEach((dependency) => {
+  devDependencies.forEach((dependency, i) => {
+    if (projectType === 'node' && i >= 6) return;
     console.log(`\t${dependency}`);
   });
   console.log('\n\tCLEAN CODING JOURNEY BEGINS!\n');
@@ -83,12 +108,17 @@ const devDependencies = [
   'eslint-config-airbnb-base@15.0.0',
   'eslint-config-prettier@8.6.0',
   'eslint-plugin-import@2.27.5',
-  'eslint-plugin-prettier@4.2.1'
+  'eslint-plugin-prettier@4.2.1',
+  'eslint-plugin-react@7.32.2',
+  'eslint-plugin-jsx-a11y@6.7.1',
+  'eslint-plugin-react-hooks@4.6.0'
 ];
 
 (async () => {
   const packageManager = await promptPackageManager();
-  await addConfigs();
-  await installDevDependencies(packageManager, devDependencies);
-  displayDevDependencies(devDependencies);
+  const projectType = await promptProjectType();
+
+  await addConfigs(projectType);
+  await installDevDependencies(packageManager, projectType, devDependencies);
+  displayDevDependencies(projectType, devDependencies);
 })();
